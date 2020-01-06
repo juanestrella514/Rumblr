c443f631b1be32d75226c855056c5346fc1994e5
unknown <juanestrella514@yahoo.com>
2020-01-05 23:33:35 -0500

    cleaned up css by making multiple css files

--- server.rb
+++ server.rb
@@ -12,27 +12,12 @@ set :port, 3000
 set :database, {adapter: "sqlite3", database: "./rumblr.sqlite3"}
 enable :sessions
 
+set :show_exceptions, :after_handler
+
 
 
 get '/' do 
     erb :home
-end
-
-get '/login' do
-    erb :login
-end
-
-#fix this post not working
-
-post '/login/' do
-    @user = User.find_by(email: params[:email])
-    given_password = params[:password]  
-    if @user.password == given_password
-        session[:user_id] = @user.id
-        
-    redirect '/profile'
-    else
-    end
 end
 
 get '/signup' do
@@ -50,12 +35,52 @@ post '/signup' do
     end
 end
 
-get '/profile' do
-    redirect '/' unless session[:user_id]
-    erb :profile
+get '/login' do
+    erb :login
 end
+
+#fix this post not working
+
+post '/login' do
+    @user = User.find_by(email: params[:email])
+    given_password = params[:password]  
+    # if @user.password == given_password
+    #     session[:user_id] = @user.id    
+    redirect '/profile'
+
+    # else
+    # end
+end
+
+
+
+# get '/profile' do
+#     redirect '/' unless session[:user_id]
+#     erb :profile
+# end
+
+get '/profile/:firstname' do  
+    @user = User.find(params[:firstname])
+    erb :profile
+    rescue ActiveRecord::RecordNotFound
+        puts "ERROR 404"
+        erb:home
+end
+
+# get '/profile/:id' do  
+#     @user = User.find(params[:id])
+#     # @dogs = @user.dogs
+#     # raise StandardError.new('This user has no dogs')
+#     erb :profile   
+# end
     
 post '/logout' do
     session.clear
     redirect '/login'
 end
+
+
+not_found do
+    '<img src="https://i.gifer.com/5Oe.gif" height="100%" width="100%">'
+end
+
