7296524be6c061e855de16fd49778613879d4111
unknown <juanestrella514@yahoo.com>
2020-01-10 12:03:23 -0500

    fixed issue with logout

--- server.rb
+++ server.rb
@@ -29,7 +29,7 @@ post '/signup' do
         @user.save
         session[:user_id] = @user.id
         session[:first_name] = @user.firstname
-        redirect %(/profile/#{@user.id})
+        redirect '/profile'
     else
         flash[:error] = @user.errors.full_messages
         redirect '/signup'
@@ -55,8 +55,8 @@ end
 
 
 get '/profile' do  
-    @user = User.find_by(id: params[:id])
-    erb :profile
+    @user = User.find_by(id: session[:user_id])
+    erb :profile, :layout => :user_layout
     # rescue ActiveRecord::RecordNotFound
     #     puts "ERROR 404"
     #     erb :profile
@@ -66,25 +66,17 @@ post '/profile' do
     @article = Article.new(title: params["title"], content: params["content"], user_id: session[:user_id])
     if @article.valid?
         @article.save
-        session[:title] = @article.title
-        session[:content] = @article.content
         redirect '/profile'
     end
 end 
 
     
-post '/logout' do
-    session.clear
+get '/logout' do
+    session[:user_id] = nil
     redirect '/login'
 end
 
 
-not_found do
-    '<img src="https://i.gifer.com/5Oe.gif" height="100%" width="100%">'
-end
-
-
-
-
-
-
+# not_found do
+#     '<img src="https://i.gifer.com/5Oe.gif" height="100%" width="100%">'
+# end
